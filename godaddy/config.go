7e5c808af608f74f89f9678f780c7a9ed8822237
Lek <lek.malaysia@gmail.com>
2023-12-12 11:40:44 +0800

    First review.

--- godaddy/config.go
+++ godaddy/config.go
@@ -2,7 +2,6 @@ package godaddy_provider
 
 import (
 	"fmt"
-	"log"
 	"terraform-provider-st-godaddy/godaddy/api"
 )
 
@@ -16,12 +15,9 @@ type Config struct {
 // Client returns a new client for accessing GoDaddy.
 func (c *Config) Client() (*api.Client, error) {
 	client, err := api.NewClient(c.BaseURL, c.Key, c.Secret)
-
 	if err != nil {
 		return nil, fmt.Errorf("error setting up client: %s", err)
 	}
-
-	log.Print("GoDaddy Client configured")
 
 	return client, nil
 }
